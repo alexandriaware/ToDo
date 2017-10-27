cbcdd017d1d347779945d19397f52a2d77ceffd8
alexandriaware <ali.mercer92@gmail.com>
2017-10-27 10:37:36 -0400

    Completed the List class

--- ToDo/Models/Item.cs
+++ ToDo/Models/Item.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -14,5 +15,9 @@ namespace ToDo.Models
         public DateTime DueDate { get; set; }
         public string ItemDetails { get; set; }
         public bool IsDone { get; set; }
+
+        [ForeignKey("List")]
+        public int ListID { get; set; }
+        public virtual List List { get; set; }
     }
 }
